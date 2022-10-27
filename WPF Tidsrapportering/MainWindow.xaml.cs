1efe0b31ab82e3ad13f92668925048c8c304e7fe
Jennifer <ki.in@live.se>
2022-10-27 14:32:55 +0200

    text

---
+++ WPF Tidsrapportering/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tidsrapportering
+{
+    public partial class MainWindow : Window
+    {
+        List<Worker> workers = new List<Worker>();
+        Worker time = new Worker(null,null,DateTime.MinValue);
+        public MainWindow()
+        {
+            InitializeComponent();
+            for(time.timestart=DateTime.Parse("00:00:00");time.timestart<=time.timeend;)
+            {
+                time.timestart=time.timestart.AddMinutes(30);
+                time.Times.Add(time.timestart.AddMinutes(30).ToShortTimeString());
+                TimeBox.ItemsSource=time.Times;
+            }
+        }
+
+        public void UpdateContent()
+        {
+            Result_ListBox.ItemsSource = null;
+            Result_ListBox.ItemsSource = workers;            
+        }
+
+        private void AddWork(object sender, RoutedEventArgs e)
+        {
+            string[] dateArray = WorkDate.Text.Split("-");
+            int[] dateInt = { Int32.Parse(dateArray[0]),
+                Int32.Parse(dateArray[1]),
+                Int32.Parse(dateArray[2]) };
+
+            string[] timeArray = TimeBox.Text.Split(":");
+            int[] timeInt = { Int32.Parse(timeArray[0]),
+                Int32.Parse(timeArray[1])};
+
+            DateTime dateWorked = new DateTime(dateInt[0],
+                 dateInt[1],
+                 dateInt[2],
+                 timeInt[0],
+                 timeInt[1],
+                 0);
+
+            workers.Add(new Worker(BoxWorkerID.Text, TextBox_Work.Text, dateWorked));
+
+            Result_ListBox.ItemsSource = workers;
+            UpdateContent();
+        }
+    }    
+}
---
+++ WPF Tidsrapportering/Worker.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Tidsrapportering
+{
+    internal class Worker
+    {
+        public List<string> Times= new List<string>();
+        public DateTime timestart = DateTime.Parse("00:00:00");
+        public DateTime timeend = DateTime.Parse("23:45:00");
+
+        string WorkerID { get; set; }
+        string Work { get; set; }
+        DateTime Workdate { get; set; }
+
+        public Worker(string workerID, string work, DateTime workdate)
+        {
+            this.WorkerID = workerID;
+            this.Work = work;
+            this.Workdate = workdate;
+        }
+
+        public override string ToString()
+        {
+            return WorkerID + " " + Workdate.ToString() + " " + Work;
+        }
+    }
+}
